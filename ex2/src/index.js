d3f320957384ef5909915a41a278d9ef07cb923f
kenjimorita <morita_kenji_gn@cyberagent.co.jp>
2016-11-19 10:32:05 +0900

    add_react env

--- ex2/src/index.js
+++ ex2/src/index.js
@@ -1,31 +1,46 @@
-var promise = function(url) {
- return new Promise(function (resolve, reject) {
-  var req = new XMLHttpRequest();
-  req.open('GET', url);
-  req.onload = function () {
-   if (req.status == 200) {
-    resolve(req.response);
-   } else {
-    reject(Error(req.statusText));
-   }
-  };
-  req.onerror = function () {
-   reject(Error("Nettwork Error"))
-  };
-  req.send();
- });
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+class Greet extends Component {
+ render() {
+
+  var promise = function(url) {
+   return new Promise(function (resolve, reject) {
+    var req = new XMLHttpRequest();
+    req.open('GET', url);
+    req.onload = function () {
+     if (req.status == 200) {
+      resolve(req.response);
+     } else {
+      reject(Error(req.statusText));
+     }
+    };
+    req.onerror = function () {
+     reject(Error("Nettwork Error"))
+    };
+    req.send();
+   });
+  }
+  promise('http://localhost:3000/posts').then(function(result){
+   var res = document.querySelector('#result')
+   var fragment = document.createDocumentFragment();
+   JSON.parse(result).slice().forEach(function(ele, i){
+    var li = document.createElement('li');
+    li.textContent += ele.comments;
+    fragment.appendChild(li);
+   });
+   res.appendChild(fragment);
+  }, function(reject){
+   console.log(reject);
+  }).catch(function(error){
+   console.log(error);
+  })
+  return (
+          <ul id="result"></ul>
+  )
+ }
 }
-promise('http://localhost:3000/posts').then(function(result){
- var res = document.querySelector('#result')
- var fragment = document.createDocumentFragment();
- JSON.parse(result).slice().forEach(function(ele, i){
-  var li = document.createElement('li');
-  li.textContent += ele.comments;
-  fragment.appendChild(li);
- });
- res.appendChild(fragment);
-}, function(reject){
- console.log(reject);
-}).catch(function(error){
- console.log(error);
+window.addEventListener('DOMContentLoaded', ()=>{
+ ReactDOM.render(<Greet />, document.querySelector('main'));
 })
+
