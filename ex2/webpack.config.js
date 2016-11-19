ead5b606d516d972b824a2d41792b063ab970304
kenjimorita <morita_kenji_gn@cyberagent.co.jp>
2016-11-19 10:06:29 +0900

    add_loader minimum

--- ex2/webpack.config.js
+++ ex2/webpack.config.js
@@ -1,13 +1,31 @@
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-        path: __dirname,
-        filename: "bundle.js"
-        
+    context: __dirname + "/src",
+    entry: {
+        javascript: "./index.js",
+        html: "./index.html",
+        css: "./index.css",
     },
+
+    output: {
+        path: __dirname + "/dist",
+        filename: "bundle.js",
+    },
+
     module: {
         loaders: [
-            { test: /\.css$/, loader: "style!css"}
-        ]
-    }
-};
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loaders: ["babel-loader"],
+            },
+            {
+                test: /\.html$/,
+                loader: "file?name=[name].[ext]",
+            },
+            {
+                test: /\.css$/,
+                loader: "file?name=[name].[ext]",
+            },
+        ],
+    },
+}
