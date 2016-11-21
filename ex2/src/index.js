5a08703e3e6b82bd919dc74e7b57ba0383636384
kenjimorita <morita_kenji_gn@cyberagent.co.jp>
2016-11-21 10:06:03 +0900

    just only write

--- ex2/src/index.js
+++ ex2/src/index.js
@@ -5,46 +5,31 @@ import 'babel-polyfill';
 
 class Greet extends Component {
  render() {
-  const sleep = (msec) => new Promise((resolve) => {
-   setTimeout(resolve, msec);
-  });
-  (async () => {
-   console.log('start');
-  await sleep(2000);
-  console.log('end');
- })();
-
-  // var promise = function(url) {
-  //  return new Promise(function (resolve, reject) {
-  //   var req = new XMLHttpRequest();
-  //   req.open('GET', url);
-  //   req.onload = function () {
-  //    if (req.status == 200) {
-  //     resolve(req.response);
-  //    } else {
-  //     reject(Error(req.statusText));
-  //    }
-  //   };
-  //   req.onerror = function () {
-  //    reject(Error('Nettwork Error'))
-  //   };
-  //   req.send();
-  //  });
-  // }
-  // promise('http://localhost:3000/posts').then(function(result){
-  //  var res = document.querySelector('#result')
-  //  var fragment = document.createDocumentFragment();
-  //  JSON.parse(result).slice().forEach(function(ele, i){
-  //   var li = document.createElement('li');
-  //   li.textContent += ele.comments;
-  //   fragment.appendChild(li);
-  //  });
-  //  res.appendChild(fragment);
-  // }, function(reject){
-  //  console.log(reject);
-  // }).catch(function(error){
-  //  console.log(error);
-  // })
+   function hidouki(url){
+    return new Promise(function(resolve, reject){
+     var req = new XMLHttpRequest();
+    req.open('GET', url);
+    req.onload = function () {
+     console.log("fafa")
+      if(req.status == 200){
+         console.log(req.response)
+       resolve(req.response);
+      } else {
+       reject(Error(req.statusText));
+      }
+     }
+     req.onerror = function(){
+       reject(Error("Network Error"))
+     }
+     req.send();
+   })
+  }
+  async function ii (){
+   var uu = await hidouki('http://localhost:3000/posts');
+   console.log(uu);
+  }
+  ii();
+  console.log("done")
   return (
           <ul id='result'></ul>
   )
